6d1b056e8f8ee7e0cee9d68923fff2f8a49a295c
james Johnson <2013matthew2013@gmail.com>
2020-01-31 01:28:45 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
+describe 'collections practice' do
+  
+def sort_array_asc
+sort_array_asc expect
+end
 
+def sort_array_desc
+sort_array_desc  
+end
+
+def sort_array_char_count
+sort_array_char_count 
+end 
+
+def swap_elements (array)
+swap_elements
+end
+
+def reverse_array (array)
+reverse_array
+end
+
+def kesha_maker (array)
+kesha_maker
+end
+
+def find_a (array)
+find_a
+end
+
+def sum_array (array)
+sum_array
+end
+
+def add_s (array)
+add_s
+end
